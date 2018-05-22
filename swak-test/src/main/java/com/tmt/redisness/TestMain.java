766707d91de5c2a07f74c8da95590dbc88df8b30
618lf@163.com <lifeng052488``>
2018-05-22 09:16:34 +0800

    删除不需要的kotlin 代码

--- swak-test/src/main/java/com/tmt/redisness/TestMain.java
+++ swak-test/src/main/java/com/tmt/redisness/TestMain.java
@@ -1,7 +1,5 @@
 package com.tmt.redisness;
 
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.redisson.config.TransportMode;
 
@@ -15,6 +13,5 @@ public class TestMain {
 		Config config = new Config();
 		config.setTransportMode(TransportMode.NIO);
 		config.useSingleServer().setAddress("redis://127.0.0.1:6379").setPassword("12345678....");
-		RedissonClient client = Redisson.create(config);
 	}
 }
