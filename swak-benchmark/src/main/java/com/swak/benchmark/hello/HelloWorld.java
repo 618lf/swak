e1c53dc2ae5af8cc3f18f340f1417dc5f61a34b2
618lf@163.com <lifeng052488``>
2018-05-23 15:49:19 +0800

    修改 benchmark 的测试

--- swak-benchmark/src/main/java/com/swak/benchmark/hello/HelloWorld.java
+++ swak-benchmark/src/main/java/com/swak/benchmark/hello/HelloWorld.java
@@ -3,6 +3,7 @@ package com.swak.benchmark.hello;
 import java.util.concurrent.TimeUnit;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
@@ -14,10 +15,15 @@ import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-@Warmup(iterations = 3) // 预热的迭代次数
+/**
+ * 修改下参数，不然每次 install 执行很长的时间
+ * @author lifeng
+ */
+@Warmup(iterations = 1) // 预热的迭代次数
 @OutputTimeUnit(TimeUnit.SECONDS)
-@Measurement(iterations = 3, time = -1, timeUnit = TimeUnit.SECONDS, batchSize = -1)
+@Measurement(iterations = 1, time = -1, timeUnit = TimeUnit.SECONDS, batchSize = -1)
 @Threads(4)
+@Fork(1)
 public class HelloWorld {
 
 	@State(Scope.Benchmark)
