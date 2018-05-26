7483b471cbf3dd179a8131369323e64f5306384e
618lf@163.com <lifeng052488``>
2018-05-26 22:45:16 +0800

    Workers 描述

--- swak-core/src/main/java/com/swak/executor/Workers.java
+++ swak-core/src/main/java/com/swak/executor/Workers.java
@@ -14,10 +14,28 @@ import reactor.core.publisher.Operators;
 import reactor.core.scheduler.Schedulers;
 
 /**
+ * https://yq.aliyun.com/articles/591627
+ * 
  * 通过线城池 executor来异步执行任务，提供不同的获取结果的方式
  * 
+ * 延迟：
  * stream 、mono     延迟执行，只有最后终端操作时才会触发整个执行链
  * future 、optional 立即执行
+ * 
+ * 可重用：
+ * future、optional、mono 可重用
+ * stream 不可重用
+ * 
+ * 异步：
+ * future、mono 异步执行
+ * stream、optional 不可异步
+ * 
+ * 推模式还是拉模式：
+ * Stream 、Optional 是拉模式的
+ * Future、mono 推模式
+ * 
+ * 重要性：
+ * Mono、Optional、future 是可以重用的
  * 
  * @author lifeng
  */
