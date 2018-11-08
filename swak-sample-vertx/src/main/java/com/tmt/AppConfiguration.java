d40fb5b4e5da4e446fddfeaa5e5f4ae2cee6de39
618lf@163.com <lifeng052488``>
2018-11-08 11:16:04 +0800

    修改vertx 的示例代码

--- swak-sample-vertx/src/main/java/com/tmt/AppConfiguration.java
+++ swak-sample-vertx/src/main/java/com/tmt/AppConfiguration.java
@@ -1,11 +1,20 @@
 package com.tmt;
 
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.swak.security.JwtAuthProvider;
+import com.swak.utils.Sets;
 import com.swak.vertx.config.IRouterConfig;
+import com.swak.vertx.config.VertxProperties;
+import com.swak.vertx.security.JwtAuthHandler;
+import com.swak.vertx.security.SecurityFilter;
+import com.swak.vertx.security.filter.Filter;
 
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.CorsHandler;
 
 /**
@@ -17,17 +26,48 @@ import io.vertx.ext.web.handler.CorsHandler;
 public class AppConfiguration {
 
 	/**
-	 * 网站自定义的路由配置
+	 * 授权提供
+	 * 
+	 * @param properties
+	 * @return
+	 */
+	@Bean
+	public JwtAuthProvider jwtAuth(VertxProperties properties) {
+		JwtAuthProvider JwtAuth = new JwtAuthProvider(properties.getKeyStorePath(), properties.getKeyStorePass(), properties.getJwtTokenName());
+		return JwtAuth;	}
+
+	/**
+	 * 安全过滤
 	 * 
 	 * @return
 	 */
 	@Bean
-	public IRouterConfig routerConfig() {
-		return new IRouterConfig() {
+	public Filter securityFilter() {
+		// 权限配置
+		SecurityFilter filter = new SecurityFilter();
+		filter.definition("/api/login=anno").definition("/api/logout=anno").definition("/api/reqister=anno")
+				.definition("/api/user/=user").definition("/api/manage/=user, role[admin]").definition("/=user");
+		return filter;
+	}
 
+	/**
+	 * 网站自定义的路由配置， 可以配置多个
+	 * 
+	 * @return
+	 */
+	@Bean
+	public IRouterConfig routerConfig(JwtAuthProvider jwtAuth, Filter securityFilter, VertxProperties properties) {
+		return new IRouterConfig() {
 			@Override
 			public void apply(Router router) {
-				router.route().handler(CorsHandler.create("*"));
+				Set<String> headers = Sets.newHashSet();
+				headers.add("X-Requested-With");
+				headers.add(jwtAuth.getTokenName());
+				router.route().handler(CorsHandler.create("*").allowedHeaders(headers));
+				router.route().handler(BodyHandler.create(properties.getUploadDirectory())
+							  .setBodyLimit(properties.getBodyLimit())
+							  .setDeleteUploadedFilesOnEnd(properties.isDeleteUploadedFilesOnEnd()));
+				router.route().handler(JwtAuthHandler.create(jwtAuth, securityFilter));
 			}
 		};
 	}
