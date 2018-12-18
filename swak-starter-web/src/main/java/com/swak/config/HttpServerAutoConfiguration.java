b9663a8ab2d1652a3620aa3a8ac99d50e7b1b8a4
618lf@163.com <lifeng052488``>
2018-12-18 16:15:41 +0800

    端口占用检测

--- swak-starter-web/src/main/java/com/swak/config/HttpServerAutoConfiguration.java
+++ swak-starter-web/src/main/java/com/swak/config/HttpServerAutoConfiguration.java
@@ -2,6 +2,8 @@ package com.swak.config;
 
 import static com.swak.Application.APP_LOGGER;
 
+import java.net.Socket;
+
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -27,14 +29,21 @@ public class HttpServerAutoConfiguration {
 
 	private HttpServerProperties properties;
 
+	/**
+	 * 端口占用检测
+	 * 
+	 * @param properties
+	 */
 	public HttpServerAutoConfiguration(HttpServerProperties properties) {
 		this.properties = properties;
+		this.adaptablePort();
 		APP_LOGGER.debug("Loading Http Server");
 	}
 
 	/**
-	 * -Dio.netty.leakDetectionLevel=PARANOID
-	 * 构建 Reactive Server ，需要提供 HttpHandler 来处理 http 请求
+	 * -Dio.netty.leakDetectionLevel=PARANOID 构建 Reactive Server ，需要提供 HttpHandler
+	 * 来处理 http 请求
+	 * 
 	 * @param handler
 	 * @return
 	 */
@@ -53,9 +62,36 @@ public class HttpServerAutoConfiguration {
 		// upload set
 		DiskFileUpload.deleteOnExitTemporaryFile = false;
 		DiskFileUpload.baseDirectory = null;
-         
+
 		// 真实的服务器，用于提供 http 服务
 		HttpServer httpServer = HttpServer.build(properties);
 		return new ReactiveServer(httpServer, handler);
+	}
+
+	/**
+	 * 适配端口
+	 */
+	private void adaptablePort() {
+		int port = properties.getPort();
+		if (port == -1) {
+			int startPort = 80;
+			while (!useable(startPort)) {
+				startPort++;
+			}
+			properties.setPort(startPort);
+		} else if (!useable(port)) {
+			throw new RuntimeException("端口占用");
+		}
+	}
+
+	// 校验是否可以用
+	private boolean useable(int port) {
+		try {
+			Socket socket = new Socket("127.0.0.1", port);
+			socket.close();
+			return false;
+		} catch (Exception e) {
+			return true;
+		}
 	}
 }
