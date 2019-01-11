841abd95ec14381b026e74e809245fbf9ef3663b
618lf@163.com <lifeng052488``>
2019-01-11 17:34:27 +0800

    5

--- swak-fx/src/main/java/com/swak/MainApplication.java
+++ swak-fx/src/main/java/com/swak/MainApplication.java
@@ -30,7 +30,7 @@ public class MainApplication extends AbstractApplication {
 	@Override
 	protected <T> T start(String[] savedArgs) {
 		try {
-			Thread.sleep(1000);
+			Thread.sleep(5000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
