1afb769d8d79fac7c85dfb253b24269eafc9bb64
618lf@163.com <lifeng052488``>
2019-03-16 18:31:32 +0800

    测试条件锁

--- swak-redis/src/test/java/com/swak/test/lock/LockTest.java
+++ swak-redis/src/test/java/com/swak/test/lock/LockTest.java
@@ -4,6 +4,7 @@ import java.util.concurrent.CountDownLatch;
 
 import org.junit.Test;
 
+import com.swak.lock.redis.ConditionRedisLock;
 import com.swak.lock.redis.OrderRedisLock;
 import com.swak.test.RedisTest;
 
@@ -26,6 +27,30 @@ public class LockTest extends RedisTest {
 		Long start_time = System.currentTimeMillis();
 		CountDownLatch latch = new CountDownLatch(10000);
 		OrderRedisLock lock = new OrderRedisLock("wx");
+		for (int i = 0; i < 10000; i++) {
+			lock.execute(() -> {
+				count++;
+				return 1;
+			}).thenAccept((v) -> {
+				latch.countDown();
+			});
+		}
+
+		latch.await();
+		Long end_time = System.currentTimeMillis();
+		System.out.println(count + "; time=" + (end_time - start_time));
+	}
+	
+	/**
+	 * 条件 - 简单的锁
+	 * 
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void conditionLock() throws InterruptedException {
+		Long start_time = System.currentTimeMillis();
+		CountDownLatch latch = new CountDownLatch(10000);
+		ConditionRedisLock lock = new ConditionRedisLock("cwx");
 		for (int i = 0; i < 10000; i++) {
 			lock.execute(() -> {
 				count++;
