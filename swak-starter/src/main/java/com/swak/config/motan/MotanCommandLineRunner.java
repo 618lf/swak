5742eb41d077e922be0d13529eb5c2a0f728b072
618lf@163.com <lifeng052488``>
2019-03-26 10:17:26 +0800

    添加motan 的限制

--- swak-starter/src/main/java/com/swak/config/motan/MotanCommandLineRunner.java
+++ swak-starter/src/main/java/com/swak/config/motan/MotanCommandLineRunner.java
@@ -3,8 +3,10 @@ package com.swak.config.motan;
 import javax.annotation.Resource;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
 
+import com.swak.motan.properties.BasicServiceConfigProperties;
 import com.swak.motan.properties.RegistryConfigProperties;
 import com.weibo.api.motan.common.MotanConstants;
 import com.weibo.api.motan.util.MotanSwitcherUtil;
@@ -16,6 +18,7 @@ import com.weibo.api.motan.util.MotanSwitcherUtil;
  * @since 2016-09-11
  */
 @Configuration
+@ConditionalOnClass({ BasicServiceConfigProperties.class })
 public class MotanCommandLineRunner implements CommandLineRunner {
 
 	/** Registry Config */
