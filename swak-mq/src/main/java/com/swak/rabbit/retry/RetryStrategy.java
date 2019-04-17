657edac1bf9ffcc9c0238e4b67b1affa138a97c3
618lf@163.com <lifeng052488``>
2019-04-17 14:56:07 +0800

    消息重试判断需要确认的消息

--- swak-mq/src/main/java/com/swak/rabbit/retry/RetryStrategy.java
+++ swak-mq/src/main/java/com/swak/rabbit/retry/RetryStrategy.java
@@ -8,6 +8,7 @@ import com.swak.rabbit.RabbitMQTemplate.ConfirmCallback;
 import com.swak.rabbit.RabbitMQTemplate.ReturnCallback;
 import com.swak.rabbit.message.Message;
 import com.swak.rabbit.message.PendingConfirm;
+import com.swak.utils.StringUtils;
 
 /**
  * 消息重试机制
@@ -18,40 +19,41 @@ public interface RetryStrategy extends ConfirmCallback, ReturnCallback {
 
 	// 专有的日志
 	Logger LOGGER = LoggerFactory.getLogger(RetryStrategy.class);
-	
+
 	/**
 	 * 绑定消息发送器
 	 * 
 	 * @param sender
 	 */
 	void bindSender(RabbitMQTemplate template);
-	
+
 	/**
 	 * 获得消息发送器
 	 * 
 	 * @param sender
 	 */
 	RabbitMQTemplate getSender();
-	
+
 	/**
 	 * 添加需要重试的消息
 	 * 
 	 * @param message
 	 */
 	void add(PendingConfirm pendingConfirm);
-	
+
 	/**
 	 * 删除已经确认的消息
 	 * 
-	 * @param id 全局的消息ID
+	 * @param id
+	 *            全局的消息ID
 	 */
 	void del(String id);
-	
+
 	/**
 	 * 确认消息发送成功，清除待重试的消息
 	 */
 	default void confirm(PendingConfirm pendingConfirm, boolean ack) {
-		if (ack) {
+		if (ack && pendingConfirm != null && StringUtils.isNotBlank(pendingConfirm.getId())) {
 			this.del(pendingConfirm.getId());
 		}
 	}
