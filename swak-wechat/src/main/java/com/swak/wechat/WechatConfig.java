f7e8521ad5911e4dd05446ef3be21fe79ee0f673
618lf@163.com <lifeng052488``>
2019-05-17 22:39:48 +0800

    先简单的处理退款，后期在研究需要添加的组件

--- swak-wechat/src/main/java/com/swak/wechat/WechatConfig.java
+++ swak-wechat/src/main/java/com/swak/wechat/WechatConfig.java
@@ -1,28 +1,19 @@
 package com.swak.wechat;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
+import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.socket.PlainConnectionSocketFactory;
-import org.apache.http.conn.ssl.DefaultHostnameVerifier;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
-import org.apache.http.util.EntityUtils;
 
 import com.swak.http.builder.RequestBuilder;
 import com.swak.utils.IOUtils;
 import com.swak.utils.Maps;
+import com.swak.utils.StringUtils;
 import com.swak.wechat.codec.SignUtils;
 
 /**
@@ -202,34 +193,71 @@ public interface WechatConfig {
 	 */
 	default CompletableFuture<String> request(String url, String data, SSLContext sslContext) {
 		return CompletableFuture.supplyAsync(() -> {
-			CloseableHttpClient httpClient = null;
+			// CloseableHttpClient httpClient = null;
+			// try {
+			// SSLConnectionSocketFactory sslConnectionSocketFactory = new
+			// SSLConnectionSocketFactory(sslContext,
+			// new String[] { "TLSv1" }, null, new DefaultHostnameVerifier());
+			//
+			// BasicHttpClientConnectionManager connManager = new
+			// BasicHttpClientConnectionManager(
+			// RegistryBuilder.<ConnectionSocketFactory>create()
+			// .register("http", PlainConnectionSocketFactory.getSocketFactory())
+			// .register("https", sslConnectionSocketFactory).build(),
+			// null, null, null);
+			// httpClient =
+			// HttpClientBuilder.create().setConnectionManager(connManager).build();
+			// HttpPost httpPost = new HttpPost(url);
+			// RequestConfig requestConfig =
+			// RequestConfig.custom().setSocketTimeout(this.getHttpReadTimeoutMs())
+			// .setConnectTimeout(this.getHttpConnectTimeoutMs()).build();
+			// httpPost.setConfig(requestConfig);
+			//
+			// StringEntity postEntity = new StringEntity(data, "UTF-8");
+			// httpPost.addHeader("Content-Type", "text/xml");
+			// httpPost.addHeader("User-Agent", "wxpay sdk java v1.0 " + this.getMchId());
+			// httpPost.setEntity(postEntity);
+			//
+			// HttpResponse httpResponse = httpClient.execute(httpPost);
+			// HttpEntity httpEntity = httpResponse.getEntity();
+			// return EntityUtils.toString(httpEntity, "UTF-8");
+			// } catch (Exception e) {
+			// return null;
+			// } finally {
+			// IOUtils.closeQuietly(httpClient);
+			// }
+
 			try {
-				SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(sslContext,
-						new String[] { "TLSv1" }, null, new DefaultHostnameVerifier());
-
-				BasicHttpClientConnectionManager connManager = new BasicHttpClientConnectionManager(
-						RegistryBuilder.<ConnectionSocketFactory>create()
-								.register("http", PlainConnectionSocketFactory.getSocketFactory())
-								.register("https", sslConnectionSocketFactory).build(),
-						null, null, null);
-				httpClient = HttpClientBuilder.create().setConnectionManager(connManager).build();
-				HttpPost httpPost = new HttpPost(url);
-				RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(this.getHttpReadTimeoutMs())
-						.setConnectTimeout(this.getHttpConnectTimeoutMs()).build();
-				httpPost.setConfig(requestConfig);
-
-				StringEntity postEntity = new StringEntity(data, "UTF-8");
-				httpPost.addHeader("Content-Type", "text/xml");
-				httpPost.addHeader("User-Agent", "wxpay sdk java v1.0 " + this.getMchId());
-				httpPost.setEntity(postEntity);
-
-				HttpResponse httpResponse = httpClient.execute(httpPost);
-				HttpEntity httpEntity = httpResponse.getEntity();
-				return EntityUtils.toString(httpEntity, "UTF-8");
+				URL URL = new URL(url);
+				HttpsURLConnection conn = (HttpsURLConnection) URL.openConnection();
+				conn.setSSLSocketFactory(sslContext.getSocketFactory());
+				conn.setConnectTimeout(this.getHttpConnectTimeoutMs());
+				conn.setReadTimeout(this.getHttpReadTimeoutMs());
+				conn.setDoOutput(true);
+				conn.setDoInput(true);
+				conn.setUseCaches(false);
+				conn.setRequestMethod("POST");
+				conn.setRequestProperty("Content-Type", "text/xml");
+				conn.setRequestProperty("User-Agent", "wxpay sdk java v1.0 " + this.getMchId());
+				OutputStream os = conn.getOutputStream();
+				os.write(StringUtils.getBytesUtf8(data));
+				os.flush();
+				IOUtils.closeQuietly(os);
+				int code = conn.getResponseCode();
+				if (code == 200) {
+					BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+					StringBuilder responseData = new StringBuilder();
+					String line = null;
+					while ((line = in.readLine()) != null) {
+						responseData.append(line);
+					}
+					IOUtils.closeQuietly(in);
+					return responseData.toString();
+				}
+				conn.disconnect();
+				return null;
 			} catch (Exception e) {
 				return null;
-			} finally {
-				IOUtils.closeQuietly(httpClient);
 			}
 		});
 	}
