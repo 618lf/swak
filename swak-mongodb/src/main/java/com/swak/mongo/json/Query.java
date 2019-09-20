fd85cdc35c3ca7503e262ae1c6ed2dd52695105f
618lf@163.com <lifeng052488``>
2019-09-20 10:39:17 +0800

    查询

--- swak-mongodb/src/main/java/com/swak/mongo/json/Query.java
+++ swak-mongodb/src/main/java/com/swak/mongo/json/Query.java
@@ -6,7 +6,9 @@ import org.bson.conversions.Bson;
 
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Projections;
+import com.swak.entity.Parameters;
 import com.swak.utils.Lists;
+import com.swak.utils.StringUtils;
 
 /**
  * 查询
@@ -28,11 +30,26 @@ public class Query {
 	 */
 	public <T> Query(T bean) {
 		Document filter = new Document(bean);
-		this.filter = Filters.and(filter);
+		if (!filter.isEmpty()) {
+			this.filter = Filters.and(filter);
+		}
 	}
 
 	// ------------- 排序 ----------------
 	private Order order;
+
+	/**
+	 * 排序
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public Query orderBy(Parameters param) {
+		if (StringUtils.isNotBlank(param.getSortField())) {
+			return this.orderBy(param.getSortField(), StringUtils.equals("ascending", param.getSortType()) ? 1 : -1);
+		}
+		return this;
+	}
 
 	/**
 	 * 排序
@@ -83,6 +100,26 @@ public class Query {
 			List<Bson> list = Lists.newArrayList(bsons);
 			list.add(this.filter);
 			this.filter = Filters.and(list.toArray(bsons));
+		}
+		return this;
+	}
+
+	/**
+	 * 添加条件
+	 * 
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	public Query and(String key, Object value) {
+		if (this.filter == null || this.filter instanceof Document) {
+			Document filter = this.filter == null ? new Document() : (Document) this.filter;
+			filter.put(key, value);
+			this.filter = filter;
+		} else {
+			Document filter = new Document();
+			filter.put(key, value);
+			return this.and(filter);
 		}
 		return this;
 	}
