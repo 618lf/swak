a6d0c6377e24dcde950e65359e83fc34f686ac09
618lf@163.com <lifeng052488``>
2019-10-08 09:01:02 +0800

    更新基础库的phone的正则表达式

--- swak-core/src/main/java/com/swak/utils/RegexUtil.java
+++ swak-core/src/main/java/com/swak/utils/RegexUtil.java
@@ -25,7 +25,7 @@ public final class RegexUtil {
 	public static final String DIGIT_REGEX = "^\\d+(\\.\\d+)?$";
 	public static final String IP_REGEX = "\\b((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\.((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\.((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\.((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\b";
 	public static final String EMAIL_REGEX = "^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$";
-	public static final String PHONE_NUM_REGEX = "^((13[0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$";
+	public static final String PHONE_NUM_REGEX = "^((13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(166)|(17[0,1,3,5,6,7,8])|(18[0-9])|(19[8|9]))\\d{8}$";
 
 	/**
 	 * 校验日期
