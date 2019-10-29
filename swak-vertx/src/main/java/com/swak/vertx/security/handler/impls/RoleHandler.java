f4635ac55fd64ed42ac8bc3ce926e9e00fb9e709
618lf@163.com <lifeng052488``>
2019-10-29 22:51:28 +0800

    添加对or权限的支持

--- swak-vertx/src/main/java/com/swak/vertx/security/handler/impls/RoleHandler.java
+++ swak-vertx/src/main/java/com/swak/vertx/security/handler/impls/RoleHandler.java
@@ -1,9 +1,11 @@
 package com.swak.vertx.security.handler.impls;
 
+import java.util.List;
 import java.util.Map;
 
 import com.swak.entity.Result;
 import com.swak.exception.ErrorCode;
+import com.swak.utils.Lists;
 import com.swak.utils.Maps;
 import com.swak.utils.StringUtils;
 import com.swak.vertx.security.Subject;
@@ -21,27 +23,46 @@ import io.vertx.ext.web.RoutingContext;
  */
 public class RoleHandler extends AdviceHandler implements PathDefinition {
 
-	private Map<String, String> params = Maps.newHashMap();
-	
+	private Map<String, Object> params = Maps.newHashMap();
+
 	/**
 	 * 判断用户需要拥有的权限
 	 * 
 	 * @param subject
 	 * @return
 	 */
+	@SuppressWarnings("unchecked")
 	public boolean isAccessDenied(RoutingContext context, Subject subject) {
-		
+
 		// 获取参数
-		String param = params.get(context.get(CHAIN_RESOLVE_PATH));
-		
-		// 配置了需要的权限
-		if (StringUtils.isNotBlank(param)) {
-			String roles = subject.getRoles();
-			return StringUtils.isNotBlank(roles) && StringUtils.contains(roles, param.toString());
+		Object param = params.get(context.get(CHAIN_RESOLVE_PATH));
+
+		// 单个权限
+		if (param != null && param instanceof String) {
+			return this.checkRole(subject, param.toString());
+		} else if (param != null && param instanceof List) {
+			List<String> roles = (List<String>) param;
+			for (String role : roles) {
+				if (this.checkRole(subject, role)) {
+					return true;
+				}
+			}
 		}
-		
+
 		// 无配置的权限则统一返回 false
 		return false;
+	}
+
+	/**
+	 * 目前使用这种简单的验证方式
+	 * 
+	 * @param subject
+	 * @param role
+	 * @return
+	 */
+	private boolean checkRole(Subject subject, String role) {
+		String roles = subject.getRoles();
+		return StringUtils.isNotBlank(roles) && StringUtils.contains(roles, role);
 	}
 
 	/**
@@ -57,10 +78,19 @@ public class RoleHandler extends AdviceHandler implements PathDefinition {
 	}
 
 	/**
-	 * 路径的配置
+	 * 暂时简单的实现： 路径的配置 <br>
+	 * a|b a权限或b权限 <br>
+	 * a,b a权限和b权限 <br>
 	 */
 	@Override
 	public void pathConfig(String path, String param) {
-		params.put(path, param);
+		List<String> orRoles = Lists.newArrayList();
+		if (StringUtils.contains(param, "|")) {
+			String[] _params = StringUtils.split("|");
+			orRoles = Lists.newArrayList(_params);
+			params.put(path, orRoles);
+		} else {
+			params.put(path, param);
+		}
 	}
 }
