d7dfe6de2973b4f4a32fd351517cf0160b023dec
618lf@163.com <lifeng052488``>
2019-12-03 10:26:59 +0800

    mq 修改

--- swak-mq/src/main/java/com/swak/rabbit/EventBus.java
+++ swak-mq/src/main/java/com/swak/rabbit/EventBus.java
@@ -573,7 +573,7 @@ public class EventBus {
 				// 执行处理器
 				return wrapper.invokeMethod(listener, method.getName(), types, args);
 			} catch (Exception e) {
-				LOGGER.error("Handler{} - Method{} Invoke Error：", listener.getClass(), method.getName(), e.getCause());
+				LOGGER.error("Handler[{}] - Method [{}] Invoke Error：", listener.getClass().getName(), method.getName(), e.getCause());
 				throw new AmqpException("处理消费事件错误");
 			}
 		}
