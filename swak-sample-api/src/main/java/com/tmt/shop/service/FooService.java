4a4b993ed3aa6f3bae56085b1b2395bfef780808
lifeng <618lf@163.com>
2020-04-02 12:03:26 +0800

    删除演示项目中的异步注解

--- swak-sample-api/src/main/java/com/tmt/shop/service/FooService.java
+++ swak-sample-api/src/main/java/com/tmt/shop/service/FooService.java
@@ -1,15 +1,11 @@
 package com.tmt.shop.service;
 
-import com.swak.flux.verticle.async.FluxAsync;
 import com.tmt.shop.entity.Foo;
-import com.weibo.api.motan.transport.async.MotanAsync;
 
 /**
  * 定义服务
  * @author lifeng
  */
-@MotanAsync
-@FluxAsync
 public interface FooService {
 	
 	String hello(String name);
--- swak-sample-vertx/src/main/java/com/tmt/api/facade/GoodsServiceFacade.java
+++ swak-sample-vertx/src/main/java/com/tmt/api/facade/GoodsServiceFacade.java
@@ -1,16 +1,12 @@
 package com.tmt.api.facade;
 
-import com.swak.annotation.FluxAsync;
 import com.tmt.api.entity.Goods;
-import com.weibo.api.motan.transport.async.MotanAsync;
 
 /**
  * 商品服务 用异步接口来约束前端，同步接口来约束后端，之後可以自動身成异步接口
  * 
  * @author lifeng
  */
-@MotanAsync
-@FluxAsync
 public interface GoodsServiceFacade {
 
 	/**
