6c77060898b8e4eabb4a711b15583be8719580f7
lifeng <618lf@163.com>
2020-04-09 09:29:21 +0800

    授权信息 改为链式操作

--- swak-vertx/src/main/java/com/swak/vertx/transport/AuthorizationInfo.java
+++ swak-vertx/src/main/java/com/swak/vertx/transport/AuthorizationInfo.java
@@ -1,9 +1,9 @@
 package com.swak.vertx.transport;
 
-import com.swak.utils.Sets;
-
 import java.io.Serializable;
 import java.util.Set;
+
+import com.swak.utils.Sets;
 
 /**
  * 得到相关的权限
@@ -13,24 +13,26 @@ import java.util.Set;
  */
 public class AuthorizationInfo implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private Set<String> permissions = Sets.newHashSet();
-    private Set<String> roles = Sets.newHashSet();
+	private Set<String> permissions = Sets.newHashSet();
+	private Set<String> roles = Sets.newHashSet();
 
-    public Set<String> getRoles() {
-        return roles;
-    }
+	public Set<String> getRoles() {
+		return roles;
+	}
 
-    public Set<String> getPermissions() {
-        return permissions;
-    }
+	public Set<String> getPermissions() {
+		return permissions;
+	}
 
-    public void addPermission(String permission) {
-        this.permissions.add(permission);
-    }
+	public AuthorizationInfo addPermission(String permission) {
+		this.permissions.add(permission);
+		return this;
+	}
 
-    public void addRole(String role) {
-        this.roles.add(role);
-    }
+	public AuthorizationInfo addRole(String role) {
+		this.roles.add(role);
+		return this;
+	}
 }
