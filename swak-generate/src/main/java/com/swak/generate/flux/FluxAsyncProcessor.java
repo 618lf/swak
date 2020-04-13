bb7b408d830caf479ee7daf86f5718085ccff0b4
lifeng <618lf@163.com>
2020-04-13 20:37:43 +0800

    自动生成异步代码的bug

--- swak-generate/src/main/java/com/swak/generate/flux/FluxAsyncProcessor.java
+++ swak-generate/src/main/java/com/swak/generate/flux/FluxAsyncProcessor.java
@@ -27,166 +27,169 @@ import java.util.concurrent.CompletableFuture;
  */
 public class FluxAsyncProcessor extends AbstractProcessor {
 
-    protected static String ASYNC = Constants.ASYNC_SUFFIX;
-    protected static String GENERATE_PATH_KEY = "fluxGeneratePath";
-    protected static String TARGET_DIR;
+	protected static String ASYNC = Constants.ASYNC_SUFFIX;
+	protected static String GENERATE_PATH_KEY = "fluxGeneratePath";
+	protected static String TARGET_DIR;
 
-    public synchronized void init(ProcessingEnvironment processingEnv) {
-        super.init(processingEnv);
-        String path = processingEnv.getOptions().get(GENERATE_PATH_KEY);// use javac complie options
-        // -AfluxGeneratePath=xxx
-        if (path != null) {
-            TARGET_DIR = path;
-        } else { // use jvm option -DfluxGeneratePath=xxx
-            TARGET_DIR = System.getProperty(GENERATE_PATH_KEY, "src/main/generated-sources/");
-        }
+	public synchronized void init(ProcessingEnvironment processingEnv) {
+		super.init(processingEnv);
+		String path = processingEnv.getOptions().get(GENERATE_PATH_KEY);// use javac complie options
+		// -AfluxGeneratePath=xxx
+		if (path != null) {
+			TARGET_DIR = path;
+		} else { // use jvm option -DfluxGeneratePath=xxx
+			TARGET_DIR = System.getProperty(GENERATE_PATH_KEY, "src/main/generated-sources/");
+		}
 
-    }
+	}
 
-    @Override
-    public SourceVersion getSupportedSourceVersion() {
-        return SourceVersion.latestSupported();
-    }
+	@Override
+	public SourceVersion getSupportedSourceVersion() {
+		return SourceVersion.latestSupported();
+	}
 
-    @Override
-    public Set<String> getSupportedAnnotationTypes() {
-        HashSet<String> types = new HashSet<>();
-        types.add(FluxAsync.class.getName());
-        return types;
-    }
+	@Override
+	public Set<String> getSupportedAnnotationTypes() {
+		HashSet<String> types = new HashSet<>();
+		types.add(FluxAsync.class.getName());
+		return types;
+	}
 
-    @Override
-    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        if (roundEnv.processingOver()) {
-            return true;
-        }
-        Set<? extends Element> elems = roundEnv.getElementsAnnotatedWith(FluxAsync.class);
-        if (elems == null || elems.isEmpty()) {
-            return true;
-        }
-        for (Element elem : roundEnv.getElementsAnnotatedWith(FluxAsync.class)) {
-            processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE,
-                    "FluxAsyncProcessor will process " + elem.toString() + ", generate class path:" + TARGET_DIR);
-            try {
-                writeAsyncClass(elem);
-                processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE,
-                        "FluxAsyncProcessor done for " + elem.toString());
-            } catch (Exception e) {
-                processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING,
-                        "FluxAsyncProcessor process " + elem.toString() + " fail. exception:" + e.getMessage());
-                e.printStackTrace();
-            }
-        }
-        return true;
-    }
+	@Override
+	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+		if (roundEnv.processingOver()) {
+			return true;
+		}
+		Set<? extends Element> elems = roundEnv.getElementsAnnotatedWith(FluxAsync.class);
+		if (elems == null || elems.isEmpty()) {
+			return true;
+		}
+		for (Element elem : roundEnv.getElementsAnnotatedWith(FluxAsync.class)) {
+			processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE,
+					"FluxAsyncProcessor will process " + elem.toString() + ", generate class path:" + TARGET_DIR);
+			try {
+				writeAsyncClass(elem);
+				processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE,
+						"FluxAsyncProcessor done for " + elem.toString());
+			} catch (Exception e) {
+				processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING,
+						"FluxAsyncProcessor process " + elem.toString() + " fail. exception:" + e.getMessage());
+				e.printStackTrace();
+			}
+		}
+		return true;
+	}
 
-    private void writeAsyncClass(Element elem) throws Exception {
+	private void writeAsyncClass(Element elem) throws Exception {
 
-        TypeElement interfaceClazz = (TypeElement) elem;
-        String className = interfaceClazz.getSimpleName().toString();
-        if (elem.getKind().isClass() && interfaceClazz.getInterfaces() != null) {
-            // Definition className with one interface
-            TypeElement de = (TypeElement) ((DeclaredType) interfaceClazz.getInterfaces().get(0)).asElement();
-            className = de.getSimpleName().toString();
-        }
+		TypeElement interfaceClazz = (TypeElement) elem;
+		String className = interfaceClazz.getSimpleName().toString();
+		if (elem.getKind().isClass() && interfaceClazz.getInterfaces() != null) {
+			// Definition className with one interface
+			TypeElement de = (TypeElement) ((DeclaredType) interfaceClazz.getInterfaces().get(0)).asElement();
+			className = de.getSimpleName().toString();
+		}
 
-        TypeSpec.Builder classBuilder = TypeSpec.interfaceBuilder(className + ASYNC).addModifiers(Modifier.PUBLIC);
+		TypeSpec.Builder classBuilder = TypeSpec.interfaceBuilder(className + ASYNC).addModifiers(Modifier.PUBLIC);
 
-        // add class generic type
-        classBuilder.addTypeVariables(getTypeNames(interfaceClazz.getTypeParameters()));
+		// add class generic type
+		classBuilder.addTypeVariables(getTypeNames(interfaceClazz.getTypeParameters()));
 
-        // is class and not interfaces
-        if (elem.getKind().isClass() && interfaceClazz.getInterfaces() == null) {
+		// is class and not interfaces
+		if (elem.getKind().isClass() && interfaceClazz.getInterfaces() == null) {
 
-            // add direct method
-            addMethods(interfaceClazz, classBuilder);
+			// add direct method
+			addMethods(interfaceClazz, classBuilder);
 
-            // add superClass methods
-            addSuperClassMethods(interfaceClazz.getSuperclass(), classBuilder);
-        }
+			// add superClass methods
+			addSuperClassMethods(interfaceClazz.getSuperclass(), classBuilder);
+		}
 
-        // is class or Interface and has interfaces
-        else {
+		// is class or Interface and has interfaces
+		else {
 
-            // add method form super interfaces
-            addSuperInterfaceMethods(interfaceClazz.getInterfaces(), classBuilder);
-        }
+			// add direct method
+			addMethods(interfaceClazz, classBuilder);
 
-        // write class
-        JavaFile javaFile = JavaFile
-                .builder(processingEnv.getElementUtils().getPackageOf(interfaceClazz).getQualifiedName().toString(),
-                        classBuilder.build())
-                .build();
+			// add method form super interfaces
+			addSuperInterfaceMethods(interfaceClazz.getInterfaces(), classBuilder);
+		}
 
-        javaFile.writeTo(new File(System.getProperty("basedir"), TARGET_DIR));
-    }
+		// write class
+		JavaFile javaFile = JavaFile
+				.builder(processingEnv.getElementUtils().getPackageOf(interfaceClazz).getQualifiedName().toString(),
+						classBuilder.build())
+				.build();
 
-    private void addMethods(TypeElement interfaceClazz, TypeSpec.Builder classBuilder) {
-        List<? extends Element> elements = interfaceClazz.getEnclosedElements();
-        if (elements != null && !elements.isEmpty()) {
-            for (Element e : elements) {
-                if (ElementKind.METHOD.equals(e.getKind())) {
-                    ExecutableElement method = (ExecutableElement) e;
+		javaFile.writeTo(new File(System.getProperty("basedir"), TARGET_DIR));
+	}
 
-                    // must public
-                    if (!method.getModifiers().contains(Modifier.PUBLIC)) {
-                        continue;
-                    }
+	private void addMethods(TypeElement interfaceClazz, TypeSpec.Builder classBuilder) {
+		List<? extends Element> elements = interfaceClazz.getEnclosedElements();
+		if (elements != null && !elements.isEmpty()) {
+			for (Element e : elements) {
+				if (ElementKind.METHOD.equals(e.getKind())) {
+					ExecutableElement method = (ExecutableElement) e;
 
-                    MethodSpec.Builder methodBuilder = MethodSpec.methodBuilder(method.getSimpleName().toString())
-                            .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
-                            .returns(getReturnType(method.getReturnType()))
-                            .addTypeVariables(getTypeNames(method.getTypeParameters()))
-                            .addAnnotations(getAnnotationSpec(method.getAnnotationMirrors()));
+					// must public
+					if (!method.getModifiers().contains(Modifier.PUBLIC)) {
+						continue;
+					}
 
-                    // add method params
-                    List<? extends VariableElement> vars = method.getParameters();
-                    for (VariableElement var : vars) {
-                        methodBuilder.addParameter(ParameterSpec
-                                .builder(TypeName.get(var.asType()), var.getSimpleName().toString()).build());
-                    }
-                    classBuilder.addMethod(methodBuilder.build());
-                }
-            }
-        }
-    }
+					MethodSpec.Builder methodBuilder = MethodSpec.methodBuilder(method.getSimpleName().toString())
+							.addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
+							.returns(getReturnType(method.getReturnType()))
+							.addTypeVariables(getTypeNames(method.getTypeParameters()))
+							.addAnnotations(getAnnotationSpec(method.getAnnotationMirrors()));
 
-    /**
-     * 返回类型
-     *
-     * @param type 类型
-     * @return 类型
-     */
-    private TypeName getReturnType(TypeMirror type) {
-        TypeName realType;
-        if (type == null || type.getKind().equals(TypeKind.VOID)) {
-            realType = ClassName.get(Void.class);
-        } else if (type.getKind().isPrimitive()) {
-            realType = ClassName.get(type).box();
-        } else {
-            realType = ClassName.get(type);
-        }
-        return ParameterizedTypeName.get(ClassName.get(CompletableFuture.class), realType);
-    }
+					// add method params
+					List<? extends VariableElement> vars = method.getParameters();
+					for (VariableElement var : vars) {
+						methodBuilder.addParameter(ParameterSpec
+								.builder(TypeName.get(var.asType()), var.getSimpleName().toString()).build());
+					}
+					classBuilder.addMethod(methodBuilder.build());
+				}
+			}
+		}
+	}
 
-    private List<TypeVariableName> getTypeNames(List<? extends TypeParameterElement> types) {
-        List<TypeVariableName> result = new ArrayList<>();
-        if (types != null && !types.isEmpty()) {
-            for (TypeParameterElement type : types) {
-                result.add(TypeVariableName.get(type));
-            }
-        }
-        return result;
-    }
+	/**
+	 * 返回类型
+	 *
+	 * @param type 类型
+	 * @return 类型
+	 */
+	private TypeName getReturnType(TypeMirror type) {
+		TypeName realType;
+		if (type == null || type.getKind().equals(TypeKind.VOID)) {
+			realType = ClassName.get(Void.class);
+		} else if (type.getKind().isPrimitive()) {
+			realType = ClassName.get(type).box();
+		} else {
+			realType = ClassName.get(type);
+		}
+		return ParameterizedTypeName.get(ClassName.get(CompletableFuture.class), realType);
+	}
 
-    /**
-     * 不支持注解的复制
-     *
-     * @param types 类型
-     * @return 注解
-     */
-    private List<AnnotationSpec> getAnnotationSpec(List<? extends AnnotationMirror> types) {
-        //		if (types != null && !types.isEmpty()) {
+	private List<TypeVariableName> getTypeNames(List<? extends TypeParameterElement> types) {
+		List<TypeVariableName> result = new ArrayList<>();
+		if (types != null && !types.isEmpty()) {
+			for (TypeParameterElement type : types) {
+				result.add(TypeVariableName.get(type));
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * 不支持注解的复制
+	 *
+	 * @param types 类型
+	 * @return 注解
+	 */
+	private List<AnnotationSpec> getAnnotationSpec(List<? extends AnnotationMirror> types) {
+		// if (types != null && !types.isEmpty()) {
 //			for (AnnotationMirror type : types) {
 //				AnnotationSpec.Builder builder = AnnotationSpec
 //						.builder((ClassName) ClassName.get(type.getAnnotationType()));
@@ -199,57 +202,57 @@ public class FluxAsyncProcessor extends AbstractProcessor {
 //				result.add(builder.build());
 //			}
 //		}
-        return new ArrayList<>();
-    }
+		return new ArrayList<>();
+	}
 
-    private void addSuperInterfaceMethods(List<? extends TypeMirror> superInterfaces, TypeSpec.Builder classBuilder) {
-        if (superInterfaces != null && !superInterfaces.isEmpty()) {
-            for (TypeMirror tm : superInterfaces) {
-                try {
-                    if (tm.getKind().equals(TypeKind.DECLARED)) {
-                        TypeElement de = (TypeElement) ((DeclaredType) tm).asElement();
-                        addMethods(de, classBuilder);
-                        addSuperInterfaceMethods(de.getInterfaces(), classBuilder);
-                    }
-                } catch (Exception e) {
-                    processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING,
-                            "FluxAsyncProcessor process superinterface " + tm.toString() + " fail. exception:"
-                                    + e.getMessage());
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+	private void addSuperInterfaceMethods(List<? extends TypeMirror> superInterfaces, TypeSpec.Builder classBuilder) {
+		if (superInterfaces != null && !superInterfaces.isEmpty()) {
+			for (TypeMirror tm : superInterfaces) {
+				try {
+					if (tm.getKind().equals(TypeKind.DECLARED)) {
+						TypeElement de = (TypeElement) ((DeclaredType) tm).asElement();
+						addMethods(de, classBuilder);
+						addSuperInterfaceMethods(de.getInterfaces(), classBuilder);
+					}
+				} catch (Exception e) {
+					processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING,
+							"FluxAsyncProcessor process superinterface " + tm.toString() + " fail. exception:"
+									+ e.getMessage());
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
-    private void addSuperClassMethods(TypeMirror superClass, TypeSpec.Builder classBuilder) {
-        if (superClass != null && !ClassName.get(superClass).equals(ClassName.OBJECT)) {
-            try {
-                if (superClass.getKind().equals(TypeKind.DECLARED)) {
-                    TypeElement de = (TypeElement) ((DeclaredType) superClass).asElement();
+	private void addSuperClassMethods(TypeMirror superClass, TypeSpec.Builder classBuilder) {
+		if (superClass != null && !ClassName.get(superClass).equals(ClassName.OBJECT)) {
+			try {
+				if (superClass.getKind().equals(TypeKind.DECLARED)) {
+					TypeElement de = (TypeElement) ((DeclaredType) superClass).asElement();
 
-                    // is class and not interfaces
-                    if (de.getKind().isClass() && de.getInterfaces() == null) {
+					// is class and not interfaces
+					if (de.getKind().isClass() && de.getInterfaces() == null) {
 
-                        // add direct method
-                        addMethods(de, classBuilder);
+						// add direct method
+						addMethods(de, classBuilder);
 
-                        // add superClass methods
-                        addSuperClassMethods(de.getSuperclass(), classBuilder);
-                    }
+						// add superClass methods
+						addSuperClassMethods(de.getSuperclass(), classBuilder);
+					}
 
-                    // is Interface or is class and has interfaces
-                    else {
-                        // add method form super interfaces
-                        addSuperInterfaceMethods(de.getInterfaces(), classBuilder);
-                    }
+					// is Interface or is class and has interfaces
+					else {
+						// add method form super interfaces
+						addSuperInterfaceMethods(de.getInterfaces(), classBuilder);
+					}
 
-                }
-            } catch (Exception e) {
-                processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING,
-                        "FluxAsyncProcessor process superClass " + superClass.toString() + " fail. exception:"
-                                + e.getMessage());
-                e.printStackTrace();
-            }
-        }
-    }
+				}
+			} catch (Exception e) {
+				processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING,
+						"FluxAsyncProcessor process superClass " + superClass.toString() + " fail. exception:"
+								+ e.getMessage());
+				e.printStackTrace();
+			}
+		}
+	}
 }
