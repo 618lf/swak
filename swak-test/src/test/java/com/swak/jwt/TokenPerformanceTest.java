194e3695bc065fe44ac01fa66960326704448110
lifeng <618lf@163.com>
2020-04-16 15:10:40 +0800

    jwt 性能测试

--- swak-test/src/test/java/com/swak/jwt/TokenPerformanceTest.java
+++ swak-test/src/test/java/com/swak/jwt/TokenPerformanceTest.java
@@ -12,7 +12,7 @@ import com.swak.test.utils.MultiThreadTest;
 public class TokenPerformanceTest {
 
 	public static void main(String[] args) {
-		JwtAuthProvider jwt = new JwtAuthProvider(null, "secret");
+		JwtAuthProvider jwt = new JwtAuthProvider(null, "13599794-749a-4290-852a-96f0d44613af");
 		JWTPayload payload = new JWTPayload();
 		payload.put("id", "1");
 		payload.put("name", "李锋");
@@ -20,13 +20,13 @@ public class TokenPerformanceTest {
 		payload = jwt.verifyToken(token);
 
 		MultiThreadTest.run(() -> {
-			for (int i = 0; i < 10000; i++) {
+			for (int i = 0; i < 1000000; i++) {
 				JWTPayload payload2 = new JWTPayload();
 				payload2.put("id", "1");
 				payload2.put("name", "李锋");
-				String token2 = jwt.generateToken(payload2);
-				payload2 = jwt.verifyToken(token2);
+				// String token2 = jwt.generateToken(payload2);
+				payload2 = jwt.verifyToken(token);
 			}
-		}, 100, "jwt性能测试");
+		}, 1, "jwt性能测试");
 	}
 }
