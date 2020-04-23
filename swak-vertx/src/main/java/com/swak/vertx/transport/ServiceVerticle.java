1144a1246cc03299aeec8d375ba2eaf32e49507a
lifeng <618lf@163.com>
2020-04-23 09:46:52 +0800

    泛型化调用后的bug

--- swak-vertx/src/main/java/com/swak/vertx/transport/ServiceVerticle.java
+++ swak-vertx/src/main/java/com/swak/vertx/transport/ServiceVerticle.java
@@ -68,7 +68,7 @@ public class ServiceVerticle extends AbstractVerticle implements Handler<Message
 		Object result = null;
 		Throwable error = null;
 		try {
-			result = wrapper.invokeMethod(service, method.getMethodName(), request.getArguments());
+			result = wrapper.invokeMethod(service, method.getMethodDesc(), request.getArguments());
 		} catch (Throwable e) {
 			error = e.getCause() != null ? e.getCause() : e;
 			logger.error("Invoke [Service:{} - Method: {}] Error.", service.getClass().getName(),
