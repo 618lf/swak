7165e63a41bbee90327b3957e013f6ac2107a0d2
lifeng <618lf@163.com>
2020-05-11 11:40:05 +0800

    app

--- swak-core/src/main/java/com/swak/App.java
+++ swak-core/src/main/java/com/swak/App.java
@@ -1,11 +1,16 @@
 package com.swak;
 
-import com.swak.incrementer.IdGen;
-import com.swak.serializer.SerializationUtils;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 
-import java.util.Map;
+import com.swak.incrementer.IdGen;
+import com.swak.serializer.SerializationUtils;
 
 /**
  * 全局 App 对象
@@ -15,117 +20,153 @@ import java.util.Map;
  */
 public final class App {
 
-    private static App ME = null;
-    private ApplicationContext context = null;
-    private String version = "1.0.0_final";
-    private String serverSn = "server-1-1";
-    private String serialization = "kryo_pool";
+	private static App ME = null;
+	private ApplicationContext context = null;
+	private String version = "1.0.0_final";
+	private String serverSn = "server-1-1";
+	private String serialization = "kryo_pool";
 
-    public ApplicationContext getContext() {
-        return context;
-    }
+	public ApplicationContext getContext() {
+		return context;
+	}
 
-    public App setContext(ApplicationContext context) {
-        this.context = context;
-        return this;
-    }
+	public App setContext(ApplicationContext context) {
+		this.context = context;
+		return this;
+	}
 
-    public String getVersion() {
-        return version;
-    }
+	public String getVersion() {
+		return version;
+	}
 
-    public App setVersion(String version) {
-        this.version = version;
-        return this;
-    }
+	public App setVersion(String version) {
+		this.version = version;
+		return this;
+	}
 
-    public String getServerSn() {
-        return serverSn;
-    }
+	public String getServerSn() {
+		return serverSn;
+	}
 
-    public App setServerSn(String serverSn) {
-        this.serverSn = serverSn;
-        return this;
-    }
+	public App setServerSn(String serverSn) {
+		this.serverSn = serverSn;
+		return this;
+	}
 
-    public String getSerialization() {
-        return serialization;
-    }
+	public String getSerialization() {
+		return serialization;
+	}
 
-    public App setSerialization(String serialization) {
-        this.serialization = serialization;
-        return this;
-    }
+	public App setSerialization(String serialization) {
+		this.serialization = serialization;
+		return this;
+	}
 
-    /**
-     * 初始化
-     */
-    public App build() {
-        IdGen.setServerSn(serverSn);
-        SerializationUtils.setSerializer(serialization);
+	/**
+	 * 初始化
+	 */
+	public App build() {
+		IdGen.setServerSn(serverSn);
+		SerializationUtils.setSerializer(serialization);
 
-        // 返回当前对象
-        ME = this;
-        return this;
-    }
+		// 返回当前对象
+		ME = this;
+		return this;
+	}
 
-    /**
-     * 当前App
-     *
-     * @return APP
-     */
-    public static App me() {
-        return ME;
-    }
+	/**
+	 * 当前App
+	 *
+	 * @return APP
+	 */
+	public static App me() {
+		return ME;
+	}
 
-    /**
-     * 获取 Bean
-     *
-     * @param name bean name
-     * @return Bean
-     */
-    @SuppressWarnings("unchecked")
-    public static <T> T getBean(String name) {
-        try {
-            return (T) ME.context.getBean(name);
-        } catch (Exception ignored) {
-        }
-        return null;
-    }
+	/**
+	 * 获取 Bean
+	 *
+	 * @param name bean name
+	 * @return Bean
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> T getBean(String name) {
+		try {
+			return (T) ME.context.getBean(name);
+		} catch (Exception ignored) {
+		}
+		return null;
+	}
 
-    /**
-     * 获取 Bean
-     *
-     * @param requiredType bean type
-     * @return Bean
-     */
-    public static <T> T getBean(Class<T> requiredType) {
-        try {
-            return ME.context.getBean(requiredType);
-        } catch (Exception ignored) {
-        }
-        return null;
-    }
+	/**
+	 * 获取 Bean
+	 *
+	 * @param requiredType bean type
+	 * @return Bean
+	 */
+	public static <T> T getBean(Class<T> requiredType) {
+		try {
+			return ME.context.getBean(requiredType);
+		} catch (Exception ignored) {
+		}
+		return null;
+	}
 
-    /**
-     * 获取 Beans
-     *
-     * @param type bean type
-     * @return Beans
-     */
-    public static <T> Map<String, T> getBeans(Class<T> type) {
-        return ME.context.getBeansOfType(type);
-    }
+	/**
+	 * 获取 Beans
+	 *
+	 * @param type bean type
+	 * @return Beans
+	 */
+	public static <T> Map<String, T> getBeans(Class<T> type) {
+		return ME.context.getBeansOfType(type);
+	}
 
-    /**
-     * 获取资源文件
-     * <p>
-     * classpath:localtion file:localtion
-     *
-     * @param localtion 资源路劲
-     * @return 资源
-     */
-    public static Resource resource(String localtion) {
-        return ME.context.getResource(localtion);
-    }
+	/**
+	 * 获取资源文件
+	 * <p>
+	 * classpath:localtion file:localtion
+	 *
+	 * @param localtion 资源路劲
+	 * @return 资源
+	 */
+	public static Resource resource(String localtion) {
+		return ME.context.getResource(localtion);
+	}
+
+	/**
+	 * 获取资源文件 -- 以文件流的方式获取（可以获取jar中的资源）
+	 *
+	 * @param localtion 资源文件
+	 * @return 流
+	 */
+	public static InputStream inputStream(String localtion) {
+		try {
+			Resource resource = App.resource(localtion);
+			if (resource.isFile()) {
+				return new FileInputStream(resource.getFile());
+			}
+			return resource.getInputStream();
+		} catch (IOException e) {
+			return null;
+		}
+	}
+
+	/**
+	 * 获取资源文件 -- 以文件的方式获取(不能获取jar中的资源)
+	 *
+	 * @param localtion 资源文件
+	 * @return 流
+	 */
+	public static File file(String localtion) {
+		try {
+			Resource resource = App.resource(localtion);
+			if (resource.isFile()) {
+				return resource.getFile();
+			}
+			return null;
+		} catch (IOException e) {
+			return null;
+		}
+	}
 }
