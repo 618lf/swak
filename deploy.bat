f341d00876bfaf77ce3b8b1aa6905f79073409da
lifeng <618lf@163.com>
2020-06-09 11:37:38 +0800

    发布到中央仓库的准备

---
+++ deploy.bat
@@ -0,0 +1,1 @@
+mvn clean deploy -projects swak-api,swak-core,swak-excel,swak-fx,swak-generate,swak-groovy,swak-http,swak-jdbc,swak-metrics,swak-mongodb,swak-mq,swak-qrcode,swak-quartz,swak-redis,swak-rxtx,swak-starter,swak-template,swak-test,swak-vertx,swak-wechat
