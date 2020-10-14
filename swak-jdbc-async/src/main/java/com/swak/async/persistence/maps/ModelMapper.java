1b0a14b13a178a3d821dbd58280c2ee8201b0951
lifeng <618lf@163.com>
2020-10-14 14:14:55 +0800

    类型转换优化

--- swak-jdbc-async/src/main/java/com/swak/async/persistence/maps/ModelMapper.java
+++ swak-jdbc-async/src/main/java/com/swak/async/persistence/maps/ModelMapper.java
@@ -1,6 +1,12 @@
 package com.swak.async.persistence.maps;
 
+import java.lang.reflect.InvocationTargetException;
+import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,8 +15,10 @@ import com.swak.async.persistence.RowMapper;
 import com.swak.async.persistence.Sql;
 import com.swak.async.persistence.define.ColumnDefine;
 import com.swak.async.persistence.define.TableDefine;
+import com.swak.utils.time.DateTimes;
 
 import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.data.Numeric;
 
 /**
  * 模型映射器
@@ -39,12 +47,60 @@ public class ModelMapper<T> implements RowMapper<T> {
 			// 属性处理
 			for (ColumnDefine column : this.table.columns) {
 				int pos = rs.getColumnIndex(column.name);
-				Object value = rs.get(column.javaField.getFieldClass(), pos);
-				column.javaField.getMethod().invoke(t, value);
+				this.handlePos(t, rs, pos, column);
 			}
 		} catch (Exception e) {
 			logger.error("解析结果错误：", e);
 		}
 		return t;
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private void handlePos(T t, Row rs, int pos, ColumnDefine column)
+			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+
+		// 数据类型
+		Class<?> fieldClass = column.javaField.getFieldClass();
+
+		// 对应的值
+		Object value = null;
+
+		// 无需转换的字段
+		if (fieldClass.isAssignableFrom(Byte.class) || fieldClass.isAssignableFrom(Short.class)
+				|| fieldClass.isAssignableFrom(Integer.class) || fieldClass.isAssignableFrom(Long.class)
+				|| fieldClass.isAssignableFrom(Double.class) || fieldClass.isAssignableFrom(Float.class)
+				|| fieldClass.isAssignableFrom(Numeric.class) || fieldClass.isAssignableFrom(LocalDate.class)
+				|| fieldClass.isAssignableFrom(Duration.class) || fieldClass.isAssignableFrom(LocalDateTime.class)) {
+
+			value = rs.get(fieldClass, pos);
+			column.javaField.getMethod().invoke(t, value);
+		}
+
+		// 处理BigDecimal
+		else if (fieldClass.isAssignableFrom(BigDecimal.class)) {
+			Numeric numeric = rs.get(Numeric.class, pos);
+			if (!(numeric == null || numeric.isNaN())) {
+				value = numeric.bigDecimalValue();
+			}
+		}
+
+		// 处理Date
+		else if (fieldClass.isAssignableFrom(Date.class)) {
+			LocalDateTime dateTime = rs.get(LocalDateTime.class, pos);
+			if (!(dateTime == null)) {
+				value = DateTimes.convertLDTToDate(dateTime);
+			}
+		}
+
+		// 处理枚举
+		else if (fieldClass.isEnum()) {
+			String string = rs.get(String.class, pos);
+			if (string != null) {
+				value = Enum.valueOf((Class) fieldClass, string);
+			}
+		}
+
+		// 设置值
+		column.javaField.getMethod().invoke(t, value);
 	}
 }
